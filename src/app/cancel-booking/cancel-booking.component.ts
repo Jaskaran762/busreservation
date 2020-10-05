4f4e8ec770fcb5bc875edc7c39f668b4505c5fdf
Dayeeta74 <dayeetasaha@gmail.com>
2020-10-05 11:07:46 +0530

    CancelBooking module completed

--- src/app/cancel-booking/cancel-booking.component.ts
+++ src/app/cancel-booking/cancel-booking.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵLocaleDataIndex } from '@angular/core';
 import { CancelBookingService } from '../cancel-booking.service';
 import { Router } from '@angular/router';
 
@@ -8,35 +8,40 @@ import { Router } from '@angular/router';
   styleUrls: ['./cancel-booking.component.css'],
 })
 export class CancelBookingComponent implements OnInit {
-  id: string;
+  id: number;
   booking: Booking = new Booking();
   customer: Customer = new Customer();
-  amount: number = 800;
+  bookingId: number = 4;
+  amount: number = 500;
   data: any;
   constructor(private cancelService: CancelBookingService) {}
 
   ngOnInit(): void {
-    this.id = sessionStorage.getItem('customerId');
+    this.id = parseInt(sessionStorage.getItem('customerId'));
   }
 
   cancel() {
     alert(
       'Your booking ' +
-        1 +
+        this.bookingId +
         ' is cancelled.  Rs.' +
         this.amount +
         ' is refunded to your wallet'
     );
     this.cancelService
-      .cancel(this.customer)
+      .cancel(this.id, this.bookingId)
       .subscribe((response) => (this.data = response));
   }
 }
 
 export class Customer {
   id: number;
+  address: string;
+  dateOfBirth: Date;
   name: string;
   email: string;
+  mobileNo: string;
+
   password: string;
 }
 
