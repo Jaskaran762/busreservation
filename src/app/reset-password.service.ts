76ae31a3442ec5a4321f01f3402e6f5b36cb497d
Dayeeta74 <dayeetasaha@gmail.com>
2020-10-07 12:24:24 +0530

    Reset password validation

---
+++ src/app/reset-password.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ResetPasswordService {
+  constructor(private http: HttpClient) {}
+
+  reset(
+    customerId: number,
+    oldPassword: string,
+    newPassword: string
+  ): Observable<any> {
+    let url =
+      'http://localhost:8181/resetPassword?&customerId=' +
+      customerId +
+      '&oldPassword=' +
+      oldPassword +
+      '&newPassword=' +
+      newPassword;
+    return this.http.get(url);
+  }
+  mail(customerId: number): Observable<any> {
+    let url = 'http://localhost:8181/forgotPassword?&customerId=' + customerId;
+    return this.http.get(url);
+  }
+}
