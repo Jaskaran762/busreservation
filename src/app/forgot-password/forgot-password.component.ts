5454a6dbe5bc4d37d8a42251350fb402568badd6
Dayeeta74 <dayeetasaha@gmail.com>
2020-10-07 16:55:24 +0530

    Forgot Password front end codes and reset password service

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,23 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import {ForgotPassword} from './ForgotPassword';
-import {Router} from '@angular/router';
+import { ForgotPassword } from './ForgotPassword';
+import { Router } from '@angular/router';
+import { ResetPasswordService } from '../reset-password.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  styleUrls: ['./forgot-password.component.css'],
 })
 export class ForgotPasswordComponent implements OnInit {
+  afterSubmit: boolean = false;
+  msg: string = 'submitted';
+  customerId: number;
+  data: number;
+  otp: number;
+  generatedOtp: number;
+  email: string;
 
-  afterSubmit:boolean = false;
-  msg:string="submitted";
+  constructor(
+    private resetPasswordService: ResetPasswordService,
+    private router: Router
+  ) {}
 
-  constructor(private router : Router) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  sendOtpMail() {
+    this.resetPasswordService
+      .searchCustomer(this.email)
+      .subscribe((response) => {
+        this.customerId = response;
+        console.log('cust id= ' + this.customerId);
+        sessionStorage.setItem('customerFromEmail', String(this.customerId));
+        if (isNaN(this.customerId)) {
+          alert('check your mail id again');
+        }
+        //if customer id is fetched, generate a OTP for the customer
+        else {
+          this.resetPasswordService
+            .mail(this.customerId)
+            .subscribe((response) => (this.generatedOtp = response));
+        }
+      });
   }
 
-  search:ForgotPassword=new ForgotPassword();
-  
+  checkOtp() {
+    console.log(this.otp);
+    console.log(this.generatedOtp);
 
+    //to check if otp matches
+    if (this.otp == this.generatedOtp) {
+      //if otp matched
+
+      this.router.navigate(['/otp']);
+    } else {
+      alert('invalid OTP');
+    }
+  }
 }
---
+++ src/app/otp/otp.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ResetPasswordService } from '../reset-password.service';
+import { PasswordValidator } from './password.validation';
+
+@Component({
+  selector: 'app-otp',
+  templateUrl: './otp.component.html',
+  styleUrls: ['./otp.component.css'],
+})
+export class OtpComponent implements OnInit {
+  customerId: number;
+  data: any;
+  newPassword: string;
+
+  OtpForm: FormGroup;
+  constructor(
+    private resetPasswordService: ResetPasswordService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
+
+  get PasswordNew() {
+    return this.OtpForm.get('password');
+  }
+  get PasswordConf() {
+    return this.OtpForm.get('confirmPassword2');
+  }
+
+  ngOnInit(): void {
+    this.customerId = parseInt(sessionStorage.getItem('customerFromEmail'));
+    //password validation
+    this.OtpForm = this.fb.group(
+      {
+        //password validation
+        password: ['', [Validators.required, Validators.minLength(8)]],
+
+        //confirm password
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validator: PasswordValidator }
+    );
+  }
+  onSubmit(): void {
+    //check if form is valid
+    if (this.OtpForm.valid) {
+      //if from is valid, reset password
+      this.resetPasswordService
+        .setPassword(this.customerId, this.newPassword)
+        .subscribe((response) => {
+          this.data = response;
+          console.log(this.data.status);
+          console.log(this.newPassword);
+          //if password was reset, navigate to home page
+          if (this.data.status == true) this.router.navigate(['/']);
+        });
+    } else {
+      alert('Please enter valid details');
+    }
+  }
+}
